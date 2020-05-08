542c41e3a35c6df933dc795f808119c05ad65ec7
Ryan Sam <ryansam.inquiries@gmail.com>
2020-05-08 01:40:21 -0500

    Added better debug error messages

--- googletokenidverifire.go
+++ googletokenidverifire.go
@@ -140,7 +140,8 @@ func choiceKeyByKeyID(keyArray []keys, tokenKid string) (keys, error) {
 			return keyArray[1], nil
 		}
 	}
-	err := errors.New("Token is not valid, kid from token and certificate does not match")
+  errorMessage := fmt.Sprintf("Token is not valid, token kid: %s from token does not match certificate kid[0]: %s or kid[1]:", tokenKid, keyArray[0].Kid, keyArray[1].Kid)
+	err := errors.New(errorMessage)
 	var nilKeys keys
 	return nilKeys, err
 }
